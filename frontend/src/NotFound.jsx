071ddcac76802c890be94a6f874e087983e9b13e
Ava Isaacson <avajeano@gmail.com>
2024-11-12 23:09:43 -0500

    production enhancements

---
+++ frontend/src/NotFound.jsx
@@ -0,0 +1,13 @@
+//** Page not found. */
+
+import React from "react";
+
+function NotFound() {
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+        </div>
+    );
+}
+
+export default NotFound;
